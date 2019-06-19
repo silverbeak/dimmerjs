23cbfc0e8baa6306c0cbeed8070f771b10962377
Kristofer Jarl <kristofer.jarl@trollmoj.com>
2019-06-19 21:32:26 +0200

    Initial code commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const _ = require('lodash')
+
+async function showSelectList(list) {
+    return await inquirer
+        .prompt([
+            {
+                type: 'checkbox',
+                message: 'Select images that you want to delete',
+                name: 'images',
+                choices: list,
+            }
+        ])
+}
+
+async function getImageList() {
+    return await exec('docker images --format "{{.ID}} {{.Repository}}"')
+}
+
+async function deleteImageList(imageIdList) {
+    const imageString = _.join(imageIdList, ' ')
+    console.log('Will delete the following images: ', imageString);
+    return await exec(`docker rmi -f ${imageString}`)
+}
+
+
+async function main() {
+    const imageObject = await getImageList()
+    console.log("List: ", imageObject.stdout);
+
+    const imageList = imageObject
+        .stdout
+        .split('\n')
+        .map(entry => {
+            const splitEntry = entry.split(' ')
+            return {
+                value: splitEntry[0],
+                name: splitEntry[1]
+            }
+        })
+
+    const imagesToDelete = await showSelectList(imageList)
+    const deleteResult = await deleteImageList(imagesToDelete.images)
+    console.log(`${deleteResult.stdout}`);
+    console.log('Done!')
+}
+
+main()
